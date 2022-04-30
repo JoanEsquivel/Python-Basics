421f87d08c4c6bc9613542d705255f012225902e
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2022-04-30 14:34:59 -0700

    First commit
    -> Hello world
    -> Basic Syntax
    -> Variable Types File

---
+++ 2-basicSyntax.py
@@ -0,0 +1,34 @@
+#https://www.tutorialspoint.com/python3/python_basic_syntax.htm
+# This is a comment
+print("-- Identation & Code Blocks --")
+if True:
+    print("Answer")
+    print("True")
+else:
+    print("False")
+
+print("------------------------------")
+
+print("-- Multiline Statements using \ --")
+total = 1 + \
+    2 + \
+    3
+print("Total")
+print(total)
+
+print("------------------------------")
+print("-- Quotation in Python using ', quotes, and triple quotes --")
+print("Using Quotes ")
+print('Using a tick')
+print("""Using Triple
+         quotes: It is made up of multiple lines and sentences.""")
+print("------------------------------")
+
+print("-- User Input  --")
+inputVar = input("\n\n Enter Something: ")
+print(inputVar)
+print("------------------------------")
+
+print("-- Demo --")
+
+print("------------------------------")
